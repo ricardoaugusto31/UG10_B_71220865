93fa2e495ae9fc3a2434a64a95986d84fd7eff0e
ricardoaugusto31 <117884134+ricardoaugusto31@users.noreply.github.com>
2022-11-24 13:01:18 +0700

    Add files via upload

---
+++ 2_B_71220865.py
@@ -0,0 +1,8 @@
+a = int(input("Masukan bilangan bulat pertama : "))
+b = int(input("Masukan bilangan bulat kedua : "))
+c = int(input("Masukan bilangan bulat ketiga : "))
+
+if c**2 == a**2+b**2 :
+    print("Merupakan Pythagoras")
+else :
+    print("Bukan Merupakan Pythagoras")
